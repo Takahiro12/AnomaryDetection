2b2b796e479dbddcf839e67426b9a4624eb792c5
Takahiro0 <50581424+Takahiro12@users.noreply.github.com>
2021-01-09 09:50:39 +0900

    chi-square distribution added.

--- ch2/example1.py
+++ ch2/example1.py
@@ -4,6 +4,7 @@ import pandas as pd
 import requests
 from statistics import mean, variance
 import matplotlib.pyplot as plt
+from scipy.stats import chi2
 
 
 def main():
@@ -17,8 +18,16 @@ def main():
     plt.show()
 
     mu = mean(df['weight'])  # 標本平均
-    s2 = mean((df['weight'] - mu)**2)
+    s2 = mean((df['weight'] - mu)**2)  # 標本分散
     print(mu, s2)
+
+    a = (df['weight'] - mu)**2/s2  # 異常度
+    th = chi2.ppf(0.99, 1)
+    plt.scatter(np.arange(0, 200), a)
+    plt.plot(np.arange(0, 200), [th] * 200, color='red', linestyle='dotted')
+    plt.xlabel('index')
+    plt.ylabel('anomaly score')
+    plt.show()
 
 
 if __name__ == '__main__':
